32fe5f9b40b3edd21019c00f06a8015d4a0ca6d3
szymon <DESKTOP-C0TU3RB>
2023-03-27 01:15:40 +0200

    Remove Dish

--- RestaurantAPI/Controllers/DishController.cs
+++ RestaurantAPI/Controllers/DishController.cs
@@ -15,12 +15,31 @@ namespace RestaurantAPI.Controllers
         {
             _dishService = dishService;
         }
-
+        [HttpDelete]
+        public ActionResult Delete([FromRoute] int restaurantId)
+        {
+            _dishService.RemoveAll(restaurantId);
+            return NoContent();
+        }
         [HttpPost]
         public ActionResult Post([FromRoute] int restaurantId, [FromBody] CreateDishDTO dto)
         {
             var newDishId = _dishService.Create(restaurantId, dto);
-            return Created($"api/{restaurantId}/dish/{newDishId}", null);
+            return Created($"api/restaurant/{restaurantId}/dish/{newDishId}", null);
         }
+        [HttpGet("{dishId")]
+        public ActionResult<DishDTO> Get([FromRoute] int restaurantId, [FromRoute] int dishId)
+        {
+            DishDTO dish = _dishService.GetById(restaurantId, dishId);
+            return Ok(dish);
+        }
+        [HttpGet]
+        public ActionResult<List<DishDTO>> Get([FromRoute] int restaurantId)
+        {
+            var result = _dishService.GetAll(restaurantId);
+            return Ok(result);
+        }
+
+
     }
 }
--- RestaurantAPI/Services/DishService.cs
+++ RestaurantAPI/Services/DishService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using RestaurantAPI.DTO;
 using RestaurantAPI.Entities;
 using RestaurantAPI.Exceptions;
@@ -8,6 +9,9 @@ namespace RestaurantAPI.Services
     public interface IDishService
     {
         int Create(int restaurantId, CreateDishDTO dto);
+        DishDTO GetById(int restaurantId, int dishId);
+        List<DishDTO> GetAll(int restaurantId);
+        void RemoveAll(int restaurantId);
     }
 
     public class DishService : IDishService
@@ -21,10 +25,8 @@ namespace RestaurantAPI.Services
         }
         public int Create(int restaurantId, CreateDishDTO dto)
         {
-            var restaurant = _context.Restaurants.FirstOrDefault(x => x.Id == restaurantId);
-            if (restaurant is null)
-              throw new NotFoundException("Restaurant not found");
-            
+            var restaurant = GetRestaurantById(restaurantId);
+
             //aby dodac nowe dania do konkretnej restauracji musimy zmapowac Dish z z CreateDishDTO
             var dishEntity = _mapper.Map<Dish>(dto);
 
@@ -36,6 +38,54 @@ namespace RestaurantAPI.Services
             return dishEntity.Id;
 
 
+        }
+
+        public DishDTO GetById(int restaurantId, int dishId)
+        {
+            var restaurant = GetRestaurantById(restaurantId);
+            //pobieranie dania z bazy
+            var dish = _context.Dishes.FirstOrDefault(d => d.Id == dishId);
+            //jesli encja nie istnieje w bazie danych albo nie jest polaczona z konkretna restauracja o ktora odpytywal klient
+            if (dish is null || dish.RestaurantId != restaurantId)
+            {
+                throw new NotFoundException("Dish not found");
+            }
+            //a jesli danie zostanie poprawnie znalezione i polaczone z konkretna restauracja mozemy je zmapowac do modelu dto.
+            //wtedy chcemy zmapowac z encji dish na model DishDTO
+            //jako parametr generyczny DishDTO a jako zrodlo encja dish
+            var dishDto = _mapper.Map<DishDTO>(dish);
+            return dishDto;
+        }
+
+        public List<DishDTO> GetAll(int restaurantId)
+        {
+
+            var restaurant = GetRestaurantById(restaurantId);
+            var dishDtos = _mapper.Map<List<DishDTO>>(restaurant.Dishes);
+
+            return dishDtos;
+        }
+
+        public void RemoveAll(int restaurantId)
+        {
+            var restaurant = GetRestaurantById(restaurantId);
+
+            //jesli restauracja istnieje, aby usunac, to na kontekscie baz danych wywolujemy metode remove range
+            _context.RemoveRange(restaurant.Dishes);
+            _context.SaveChanges();
+        }
+        private Restaurant GetRestaurantById(int restaurantId)
+        {
+            var restaurant = _context
+               .Restaurants
+               //chcemy tutaj wywolac dania z bazy danych, za pomoca include
+               .Include(r => r.Dishes)
+               .FirstOrDefault(r => r.Id == restaurantId);
+            if (restaurant is null)
+
+                throw new NotFoundException("Restaurant not found");
+
+            return restaurant;
         }
     }
 }
