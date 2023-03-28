bab371a43444e5eb3595981ef1baa21a6c8d935a
szymon <DESKTOP-C0TU3RB>
2023-03-28 03:27:35 +0200

    Created Entites User and Roles for Authentication

--- RestaurantAPI/DTO/CreateRestaurantDTO.cs
+++ RestaurantAPI/DTO/CreateRestaurantDTO.cs
@@ -7,7 +7,7 @@ namespace RestaurantAPI.DTO
         [Required]
         [MaxLength(25)]
         public string Name { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; }
         public string? Category { get; set; }
         public bool HasDelivery { get; set; }
         public string? Email { get; set; }
--- RestaurantAPI/DTO/RestaurantDTO.cs
+++ RestaurantAPI/DTO/RestaurantDTO.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; }
         public string? Category { get; set; }
         public bool HasDelivery { get; set; }
         public string City { get; set; }
